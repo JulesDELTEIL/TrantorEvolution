8771afa0b0a143ae970d2c1137e7942325cd671e
Calvier Kerwan <kerwan.calvier@epitech.eu>
2025-06-11 15:15:38 +0200

    feat(visual): added texture path for clay, stone and metal, in the resource constructor

--- gui/include/visual/drawables/ResourceNode.hpp
+++ gui/include/visual/drawables/ResourceNode.hpp
@@ -23,9 +23,9 @@ class ResourceNode : public AAnimated {
 
 static const std::map<ResourceType_e, std::string> RESOURCE_NODE_TEXTURE  = {
     {WOOD, "assets/ressources/Grass_Biom_Tree.png"},
-    {STONE, "assets/ressources/Grass_Biom_Tree.png"},
-    {CLAY, "assets/ressources/Grass_Biom_Tree.png"},
-    {METAL, "assets/ressources/Grass_Biom_Tree.png"},
+    {STONE, "assets/ressources/Stone.png"},
+    {CLAY, "assets/ressources/Clay.png"},
+    {METAL, "assets/ressources/Metal.png"},
     {OIL, "assets/ressources/Grass_Biom_Tree.png"},
     {ANTI_MATTER, "assets/ressources/Grass_Biom_Tree.png"}
 };
