dd186c32c44471c9e812616835877fc5d22d461e
celianrag <celian.raguin@epitech.eu>
2025-06-13 15:08:25 +0200

    feat(communication): adding encapsulation for socket module to manage server messages and sending some back

---
+++ ia/server_manager.py
@@ -0,0 +1,47 @@
+#
+## EPITECH PROJECT, 2025
+## TrantorEvolution
+## File description:
+## Server Manager
+#
+
+import socket
+import sys
+
+BUFF_SIZE = 1024
+INTERLOCUTOR = 0
+MESSAGE_CONTENT = 1
+WORD_COUNT = 2
+
+class ServerManager :
+    def __init__(self, host : str, port : int) :
+        self.host = host
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((self.host, self.port))
+        self.last_message = ""
+
+    def send(self, message) :
+        self.sock.send(message)
+
+    def recv(self) :
+        message = ""
+        while not ("\n" in message) :
+            message += str(self.sock.recv(1))
+        self.last_message = message
+        return message
+
+    def close(self) :
+        self.sock.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3 :
+        exit(84)
+    server_manager = ServerManager(sys.argv[1], int(sys.argv[2]))
+    print("Server manager")
+    while(True) :            print("stopping circ with", message, ("\n" in test))
+        print("starting receiving")
+        print(server_manager.recv())
+        print("Finished receiving")
+
+
