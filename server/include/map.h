25bcc9cf83a076d42cca7af61e3bde6f942a380f
luigi gomes <luigi.gomes@epitech.eu>
2025-06-16 11:06:27 +0200

    feat(map.h): adding define for ressoursces density

--- server/include/map.h
+++ server/include/map.h
@@ -8,6 +8,14 @@
 #ifndef MAP_H_
     #define MAP_H_
 
+#define FOOD_DENS 0.5
+#define WOOD_DENS 0.3
+#define ROCK_DENS 0.15
+#define CLAY_DENS 0.1
+#define METAL_DENS 0.1
+#define PETROL_DENS 0.08
+#define ANTIMATTER_DENS 0.05
+
 enum biome_e {
     SEA = 0,
     FOREST,
@@ -32,6 +40,6 @@ typedef struct map_s {
 } map_t;
 
 /*init the map depending of the width and heigt*/
-map_t **init_map(int X, int Y)
+map_t **init_map(int X, int Y);
 
 #endif /* !MAP_H_ */
