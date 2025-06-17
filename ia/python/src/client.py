71199c3fb6135288440c7a9565a57258212ca7c4
celianrag <celian.raguin@epitech.eu>
2025-06-17 19:46:03 +0200

    feat(client): adding variable player number and dimesion to get them from the server

--- ia/python/src/client.py
+++ ia/python/src/client.py
@@ -7,36 +7,59 @@
 
 import socket
 import subprocess
+from curses.ascii import isdigit
+from sqlite3 import connect
+
 from src.roles.role_map import ROLE_MAP
 from src.utils import recv_until_newline
 from src.action import Action
+from src.server_manager import ServerManager
 
-class Trantorian:
+DIMENSION_X = 0
+DIMENSION_Y = 1
+PLAYER_LEFT = 1
+
+def get_dimension(dimension_str) :
+    dimension_split = dimension_str.split()
+    if len(dimension_split) != 2 or not all([val.isdigit() for val in dimension_split]) :
+        raise Exception("Invalid dimension from server")
+    dimension_tuple = (int(dimension_split[DIMENSION_X]), int(dimension_split[DIMENSION_Y]))
+    return dimension_tuple
+
+def get_client_num(client_num_str) :
+    client_num_strip = client_num_str.strip()
+    if not client_num_strip.isdigit() :
+        raise Exception("Invalid client number left from server")
+    return int(client_num_strip)
+
+class Trantorian (ServerManager) :
     def __init__(self, host, port, team_name):
+        ServerManager.__init__(self, host, port)
         self.host = host
         self.port = port
         self.team_name = team_name
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.dimension = None
+        self.player_num = None
         self.player = None
+        self.connect()
         
     def connect(self):
-        self.sock.connect((self.host, self.port))
-        welcome = recv_until_newline(self.sock)
+        welcome = self.recv()
         if welcome.strip() != "WELCOME":
             raise ConnectionError("Expected WELCOME message.")
         self.sock.sendall(f"{self.team_name}\n".encode())
-        dimension = recv_until_newline(self.sock) #je ne crois pas qu'on en ait besoin pour le moment mais c'est dans le protocole
+        self.player_num = get_client_num(self.recv())
+        self.dimension = get_dimension(self.recv()) #je ne crois pas qu'on en ait besoin pour le moment mais c'est dans le protocole
 
     def run(self):
-        #self.connect()
         while True:
             if self.player is not None:
                 actions = self.player.decide_action()
                 for action in actions:
                     if action != Action.NONE:
                         self.sock.sendall((action + "\n").encode())
-            #response = recv_until_newline(self.sock)
-            #self.handle_response(response.strip())
+            # response = recv_until_newline(self.sock)
+            # self.handle_response(response.strip())
 
     def handle_response(self, response):
         if self.player is None:
