7befb0278bfa7bdad567dc93dd3fbd65efbc1d26
celianrag <celian.raguin@epitech.eu>
2025-06-17 13:22:15 +0200

    feat(server_manager): switching from a byte by byte buffer to a circular buffer

--- ia/python/server_manager.py
+++ ia/python/server_manager.py
@@ -6,6 +6,7 @@
 #
 
 import socket
+from collections import deque
 import sys
 
 BUFF_SIZE = 1024
@@ -26,24 +27,46 @@ class ServerManager :
 
     def recv(self) :
         message = ""
-        while not ("\n" in message) :
-            message += self.sock.recv(1).decode("utf-8")
-        self.last_message = message
+        while not "\n" in message :
+            self.sock.setblocking(True)
+            print("waiting...")
+            data = self.sock.recv(BUFF_SIZE)
+            self.sock.setblocking(False)
+            while True :
+                try :
+                    if not data :
+                        break
+                    message += data.decode("utf-8")
+                    data = self.sock.recv(BUFF_SIZE)
+                except BlockingIOError :
+                    break
+            if not data :
+                return None
         return message
 
     def close(self) :
         self.sock.close()
 
 if __name__ == "__main__":
+    server_message = ""
     if len(sys.argv) != 3 :
         exit(84)
     server_manager = ServerManager(sys.argv[1], int(sys.argv[2]))
     print("Server manager")
     while(True) :
-        print("starting receiving")
-        server_message = server_manager.recv()
-        print(server_message)
-        server_manager.send("idn ia\n".encode(encoding="utf-8"))
-        print("Finished receiving")
+        print("Starting receiving")
+        server_message += server_manager.recv()
+        if not server_message:
+            exit(84)
+        print("Receiving :", server_message)
+        while server_message :
+            index = server_message.find("\n")
+            if index == -1:
+                break
+            print("Index :", index)
+            print("MESSAGE :", server_message[:index + 1])
+            print("FROM SOURCE", server_message)
+            server_message = server_message[index + 1:]
+        print("Finished receiving with [", server_message, "] Left\n")
 
 
