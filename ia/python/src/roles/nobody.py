f301efde58287caca30101ebf1643ffe0df12226
PierrePruvost03 <pierre.pruvost@epitech.eu>
2025-06-19 13:46:10 +0200

    fix(ia-algo): remove useless variable and wrong naming

--- ia/python/src/roles/nobody.py
+++ ia/python/src/roles/nobody.py
@@ -9,11 +9,10 @@ from itertools import cycle
 from src.roles.base_role import BaseRole
 from src.action import Commands, Action
 
-class Unknown(BaseRole):
+class Nobody(BaseRole):
     def __init__(self):
         super().__init__()
 
     def decide_action(self):
         if self.queue.empty():
-            self.queue.appendleft(Commands.LEFT)
-        self.cycle += 1
+            self.queue.appendleft(Commands(Action.LEFT))
