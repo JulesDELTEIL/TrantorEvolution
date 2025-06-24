362523f45953c730e6138ede18ce505ba6224de6
baptiste <baptiste.dunes@epitech.eu>
2025-06-24 14:43:10 +0200

    feat(server-commands): added command incantation

--- server/src/commands/ai/incantation.c
+++ server/src/commands/ai/incantation.c
@@ -13,14 +13,44 @@
 #include "transmission.h"
 #include "commands.h"
 #include "actions.h"
+#include "incantation.h"
+
+static int get_nb_tile_players(player_t *head, pos_t tilepos)
+{
+    int res = 0;
+
+    while (head != NULL) {
+        if (head->pos.x == tilepos.x && head->pos.y == tilepos.y)
+            res++;
+        head = head->next;
+    }
+    return res;
+}
+
+static const bool level_up_ok(serverdata_t *sdata, player_t *player)
+{
+    int nb_players = get_nb_tile_players(sdata->game_data.players,
+        player->pos);
+    tile_t tile = sdata->game_data.map.tiles[player->pos.x][player->pos.y];
+
+    if (nb_players < ELEVATION_REQ[player->level].players)
+        return false;
+    for (uint_t k = 0; k < NB_RESOURCES; k++)
+        if (tile.resources[k] < ELEVATION_REQ[player->level].resources[k])
+            return false;
+    return true;
+}
 
 // ACTION
 int action_incantation(serverdata_t *sdata, fdarray_t *fdarray,
     client_t *client, char *data)
 {
-    char answer[BUFSIZ] = {0};
+    char answer[BUFFSIZE] = {0};
 
-    send_data(client, "ok", NULL, sdata->debug);
+    if (level_up_ok(sdata, client->player))
+        client->player->level += 1;
+    sprintf(answer, "Current level: %d", client->player->level);
+    send_data(client, answer, NULL, sdata->debug);
 }
 
 // COMMAND
@@ -31,10 +61,15 @@ int cmd_incantation(serverdata_t *sdata, fdarray_t *fdarray,
         send_data(client, "ko", NULL, sdata->debug);
         return EXIT_FAILURE;
     }
-    client->player->action.cmd = strdup(ACTIONS_ARR[INCANTATION].name);
-    client->player->action.data = strdup(data);
-    client->player->action.status = ONGOING;
-    client->player->action.end = set_timer_end(sdata->args->freq,
-        ACTIONS_ARR[INCANTATION].delay);
+    if (level_up_ok(sdata, client->player)) {
+        client->player->action.cmd = strdup(ACTIONS_ARR[INCANTATION].name);
+        client->player->action.data = strdup(data);
+        client->player->action.status = ONGOING;
+        client->player->action.end = set_timer_end(sdata->args->freq,
+            ACTIONS_ARR[INCANTATION].delay);
+        send_data(client, "Elevation underway", NULL, sdata->debug);
+    } else {
+        send_data(client, "ko", NULL, sdata->debug);
+    }
     return EXIT_SUCCESS;
 }
