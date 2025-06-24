325be6f4fada9931982517b71a586c433bcf9b41
Calvier Kerwan <kerwan.calvier@epitech.eu>
2025-06-24 14:49:37 +0200

    fix(gui-visual-tiles): fixed new asset texture  rect

--- gui/include/visual/entities/Tile.hpp
+++ gui/include/visual/entities/Tile.hpp
@@ -10,6 +10,7 @@
 
     #include <memory>
     #include <map>
+    #include <SFML/System/Clock.hpp>
     #include <SFML/Graphics/Rect.hpp>
     #include <SFML/Graphics/RenderTarget.hpp>
 
@@ -23,12 +24,12 @@ namespace visual {
    #define NB_TYPE 3
 
 static const std::map<biome_e, sf::IntRect> TEXTURE_RECT = {
-    {SEA, sf::IntRect(64, 0, 32, 32)},
-    {FOREST, sf::IntRect(128, 0, 32, 32)},
-    {MOUNTAINS, sf::IntRect(96, 0, 32, 32)},
-    {PLAINS, sf::IntRect(0, 0, 32, 32)},
-    {BEACH, sf::IntRect(32, 0, 32, 32)},
-    {EMPTY, sf::IntRect(160, 0, 32, 32)}
+    {PLAINS, sf::IntRect(0, 0, 96, 96)},
+    {BEACH, sf::IntRect(96, 0, 96, 96)},
+    {SEA, sf::IntRect(192, 0, 96, 96)},
+    {MOUNTAINS, sf::IntRect(288, 0, 96, 96)},
+    {FOREST, sf::IntRect(384, 0, 96, 96)},
+    {EMPTY, sf::IntRect(480, 0, 96, 96)}
 };
 
 class Tile {
@@ -36,7 +37,7 @@ class Tile {
         Tile(const sf::Vector2f& pos, biome_e type);
         ~Tile() = default;
 
-        void draw(sf::RenderTarget&);
+        void draw(sf::RenderTarget&, const sf::Clock&);
 
     private:
         Drawable _biome;
