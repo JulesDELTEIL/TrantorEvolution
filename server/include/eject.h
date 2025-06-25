36cae25b8efa8573cac82c1ea914c1473c1e4e88
baptiste <baptiste.dunes@epitech.eu>
2025-06-26 00:51:47 +0200

    feat(server-commands): added command eject

---
+++ server/include/eject.h
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2025
+** TrantorEvolution
+** File description:
+** eject
+*/
+
+#ifndef EJECT_H_
+    #define EJECT_H_
+
+    #include "pos.h"
+
+static const pos_t EJECT_DIR[] = {
+    {0, 0},
+    {0, -1},
+    {1, 0},
+    {0, 1},
+    {-1, 0}
+};
+
+#endif /* !GAME_H_ */
