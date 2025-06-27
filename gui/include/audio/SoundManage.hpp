85ac2e09ac88efa763a9fb690c2453504a839135
celianrag <celian.raguin@epitech.eu>
2025-06-27 17:12:01 +0200

    feat(ambiant_sound): adding map that associates biom to audio_path files (still neutral)

--- gui/include/audio/SoundManage.hpp
+++ gui/include/audio/SoundManage.hpp
@@ -9,9 +9,21 @@
     #define SOUNDMANAGER
 
 #include <iostream>
+#include <map>
 #include <SFML/Audio.hpp>
 
+#include "map_tools.h"
+
 namespace gui {
+    const std::map<biome_e, std::string> biome_map =
+    {
+        {SEA, "../../assets/Trantor.mp3"},
+        {FOREST, "../../assets/Trantor.mp3"},
+        {MOUNTAINS, "../../assets/Trantor.mp3"},
+        {PLAINS, "../../assets/Trantor.mp3"},
+        {BEACH, "../../assets/Trantor.mp3"},
+        {EMPTY, "../../assets/Trantor.mp3"}
+    };
     class SoundManage {
     public:
         SoundManage();
