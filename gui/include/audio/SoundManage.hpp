7da3b980b2936ea6451d65edc17450e3a9b93b5e
celianrag <celian.raguin@epitech.eu>
2025-06-27 18:43:52 +0200

    feat(soundManage): changing the way sound manage works to play any music and adding the possibility to switch music, and so changing its declaration in engine.cpp

--- gui/include/audio/SoundManage.hpp
+++ gui/include/audio/SoundManage.hpp
@@ -15,19 +15,19 @@
 #include "map_tools.h"
 
 namespace gui {
-    const std::map<biome_e, std::string> biome_map =
+    const std::map<biome_e, std::string> song_map =
     {
-        {SEA, "../../assets/Trantor.mp3"},
-        {FOREST, "../../assets/Trantor.mp3"},
-        {MOUNTAINS, "../../assets/Trantor.mp3"},
-        {PLAINS, "../../assets/Trantor.mp3"},
-        {BEACH, "../../assets/Trantor.mp3"},
-        {EMPTY, "../../assets/Trantor.mp3"}
+        {SEA, "assets/audio/wave.mp3"},
+        {FOREST, "assets/audio/leaf_wind.mp3"},
+        {MOUNTAINS, "assets/audio/wind.mp3"},
+        {PLAINS, "assets/audio/gazoulli.mp3"},
+        {BEACH, "assets/audio/beach.mp3"},
+        {EMPTY, ""}
     };
     class SoundManage {
     public:
-        SoundManage();
-
+        SoundManage() = default;
+        void playSong(std::string song);
     private:
         sf::SoundBuffer sb_song;
         sf::Music s_song;
