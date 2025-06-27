1c3a738bd739e35392e4ffbbf2d0380757c41d83
luigi gomes <luigi.gomes@epitech.eu>
2025-06-27 14:30:41 +0200

    feat(song): adding trantor song in soundmanage

--- gui/include/audio/SoundManage.hpp
+++ gui/include/audio/SoundManage.hpp
@@ -15,10 +15,10 @@ namespace gui {
     class SoundManage {
     public:
         SoundManage();
-    
+
     private:
         sf::SoundBuffer sb_song;
-        sf::Sound s_song;
+        sf::Music s_song;
     };
 }
 
--- gui/src/audio/SoundManage.cpp
+++ gui/src/audio/SoundManage.cpp
@@ -9,7 +9,9 @@
 
 gui::SoundManage::SoundManage()
 {
-    if (!sb_song.loadFromFile("../../assets/Trantor.mp3")) {
+    if (!s_song.openFromFile("assets/Trantor.mp3")) {
         std::cout << "[ERROR] Trantor.mp3 didn't load" << std::endl;
     }
+    s_song.setLoop(true);
+    s_song.play();
 }
